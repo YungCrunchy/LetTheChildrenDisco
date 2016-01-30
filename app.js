723e32bc1b550cdadc4508c83bb3feecc1431874
John Slevinsky <john.slev@gmail.com>
2016-01-30 15:41:20 -0700

    real peerjs setup

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: 9000, path: '/peer'});
 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+var server = require('http').createServer(app);
+var options = {debug: true};
+app.use('/peerjs', ExpressPeerServer(server, options));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
