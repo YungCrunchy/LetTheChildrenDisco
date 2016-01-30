f5459425556dc5e438a363452f6091ff17443e58
John Slevinsky <john.slev@gmail.com>
2016-01-30 14:44:50 -0700

    player control from js; learn how 2 jquery

--- public/js/player.js
+++ public/js/player.js
@@ -1,8 +1,12 @@
 /**
  * Created by john on 2016-01-30.
  */
+var $ = require('jQuery');
 
-var myaudio = new Audio("http://webaudioapi.com/samples/audio-tag/chrono.mp3");
+var myaudio = new Audio();
+myaudio = document.getElementById(player);
 
 myaudio.loop = true;
+
+myaudio.pause();
 //myaudio.addEventListener('ended', )
