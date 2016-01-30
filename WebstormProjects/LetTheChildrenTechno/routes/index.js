344291f3d308af83e7faae2d433a99f523f396ae
John Slevinsky <john.slev@gmail.com>
2016-01-30 12:43:02 -0700

    lol I am a noob at this. Add some routes to pages from main.

--- WebstormProjects/LetTheChildrenTechno/routes/index.js
+++ WebstormProjects/LetTheChildrenTechno/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Let The Children Techno' });
 });
+
+router.get('/client', function(req, res, next ){
+  res.render('client', {title: 'Client'})
+});
+
+router.get('/broadcaster', function(req, res, next){
+  res.render('broadcaster', {title: 'stream music'})
+})
 
 module.exports = router;
