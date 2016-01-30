ba4ffdede7d7f5caa03feb4f5d6cbad84e7f6962
John Slevinsky <john.slev@gmail.com>
2016-01-30 14:14:12 -0700

    player....

---
+++ public/js/player.js
@@ -0,0 +1,8 @@
+/**
+ * Created by john on 2016-01-30.
+ */
+
+var myaudio = new Audio("http://webaudioapi.com/samples/audio-tag/chrono.mp3");
+
+myaudio.loop = true;
+//myaudio.addEventListener('ended', )
